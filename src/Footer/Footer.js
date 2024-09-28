2562e32462f2da960376100bc106bcb4c0f3f388
mythri-3 <mgudiban@uncc.edu>
2024-09-27 21:04:39 -0400

    Brought PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from'react';
+
+
+function Footer() {
+  return (
+    <footer className="bottom">
+    <div className="center" role="copyrights"> 
+        All rights reserved &copy; Mythri
+    </div>
+</footer>
+  );
+}
+
+export default Footer;
